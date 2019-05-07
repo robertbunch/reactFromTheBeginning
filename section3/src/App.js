12bbcc1d56dd2a52f4101ab72b65985782cdf24c
Robert Bunch <rdbunch@gmail.com>
2019-05-07 14:56:24 -0400

    a

--- section3/src/App.js
+++ section3/src/App.js
@@ -7,7 +7,12 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      temp: ""
+      temp: "",
+      cityName: "",
+      weather: "",
+      high: "",
+      low: "",
+      icon: "",
     }
   }
 
@@ -15,15 +20,36 @@ class App extends Component {
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d'
     axios.get(url).then((resp)=>{
       this.setState({
-        temp: resp.data.main.temp
+        temp: resp.data.main.temp,
+        high: resp.data.main.temp_max,
+        low: resp.data.main.temp_min,
+        weather: resp.data.weather[0].description,
+        icon: resp.data.weather[0].icon,
+        cityName: resp.data.name
       })
-    })        
+    })    
+    var elems = document.querySelectorAll('.modal');
+    var instances = window.M.Modal.init(elems);    
   }
 
   render(){
+    const iconUrl = `http://openweathermap.org/img/w/${this.state.icon}.png`
+
     return (
       <div className="App">
         <h1>{this.state.temp}</h1>
+        <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Details</a>
+
+        <div id="modal1" className="modal">
+          <div className="modal-content">
+            <h4>{this.state.cityName}</h4>
+            <p>High: {this.state.high} - Low: {this.state.low}</p>
+            <p>{this.state.weather} <img src={iconUrl} /></p>
+          </div>
+          <div className="modal-footer">
+            <a href="#!" className="modal-close waves-effect waves-green btn-flat">Agree</a>
+          </div>
+        </div>        
       </div>
     );
   }
