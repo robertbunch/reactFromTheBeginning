9278d19f50cf8b3811c0613b2c62b5c172baa9bd
Robert Bunch <rdbunch@gmail.com>
2019-06-05 22:26:52 -0400

    Add router app

--- router-101/src/App.js
+++ router-101/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Home = ()=> <h1>Home</h1>
+const About = ()=> <h1>About</h1>
 
 function App() {
   return (
-    <div className="App">
-      <h1>Sanity Check</h1>
-    </div>
+    <Router>
+        <h1>Header</h1>
+        <div>
+          {/* we dont use a tags. that's so 2014 */}
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <h1>Footer</h1>
+    </Router>
   );
 }
 
