c4589f76eb66182f3341d70bc5ec96321f763754
Robert Bunch <rdbunch@gmail.com>
2019-11-21 16:36:12 -0500

    Add stages

---
+++ airbnb-stages/src-beginning/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component{
+
+    render(){
+        return(<>
+            <h1>Home Page</h1>
+        </>
+        )
+    }
+}
+
+export default Home;
---
+++ airbnb-stages/src-beginning/utility/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+
+class NavBar extends Component{
+
+    render(){
+        return(<>
+            <h1>NavBar Page</h1>
+        </>
+        )
+    }
+}
+
+export default NavBar;
