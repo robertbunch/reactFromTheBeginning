92f43e728435115c19844279ef4ea12516634985
Robert Bunch <rdbunch@gmail.com>
2019-11-21 16:31:28 -0500

    Update readme with routes

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -3,15 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import './App.css';
 import Home from './pages/Home/Home';
 import NavBar from './utility/NavBar/NavBar';
-// Routes:
-// /cities/recommended
-// /cities/:feature
-// /city/:cityName
-// /venues/recommended
-// /venues/superHost
-// /seach/:searchTerm
-// /venue/:venueId
-// /payment/stripe
 
 class App extends Component{
 
