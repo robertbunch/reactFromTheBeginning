600d822cd77ec5c2292f90685ce10e109cd910e9
Robert Bunch <rdbunch@gmail.com>
2020-02-10 23:30:15 -0500

    Signup and Login components

--- airbnb-clone/src/pages/Login/Login.js
+++ airbnb-clone/src/pages/Login/Login.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './Login.css';
-
 class Login extends Component{
 
     render(){
@@ -9,11 +8,15 @@ class Login extends Component{
                 <form onSubmit={this.submitLogin}>
                     <button className="facebook-login">Connect With Facebook</button>
                     <button className="google-login">Connect With Google</button>
-                    <span>or</span>
-                    <input type="text" placeholer="Email Address" />
-                    <input type="password" className="password-signup" placeholder="Passord" />
-                    <button className="sign-up-button">Log in</button>
-
+                    <div className="login-or center">
+                        <span>or</span>
+                        <div className="or-divider"></div>
+                    </div>
+                    <input type="text" className="browser-default" placeholder="Email address" />
+                    <input type="password" className="browser-default" placeholder="Password" />
+                    <button className="sign-up-button">Login</button>
+                    <div className="divider"></div>
+                    <div>Don't have an account? Sign up</div>
                 </form>
             </div>
         )
--- airbnb-clone/src/pages/Login/SignUp.js
+++ airbnb-clone/src/pages/Login/SignUp.js
@@ -4,7 +4,20 @@ class SignUp extends Component{
 
     render(){
         return(
-            <h1>SignUp</h1>
+            <div className="login-form">
+                <form onSubmit={this.submitLogin}>
+                    <button className="facebook-login">Connect With Facebook</button>
+                    <button className="google-login">Connect With Google</button>
+                    <div className="login-or center">
+                        <span>or</span>
+                        <div className="or-divider"></div>
+                    </div>
+                    <button className="sign-up-button">Sign up with email</button>
+                    <div className="divider"></div>
+                    <div>Already have an account? Log in</div>
+                </form>
+            </div>
+
         )
     }
 
