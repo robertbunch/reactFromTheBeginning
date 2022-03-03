b9dd8969fd46cb70b5bdba4450764bac8f5f5d4d
Robert Bunch <rdbunch@gmail.com>
2022-03-03 12:09:42 -0500

    Update all images again

--- starterFiles/cards.js
+++ starterFiles/cards.js
@@ -2,23 +2,23 @@ const data = [
     {
         course: "React From the Beginning",
         instructor: "Robert Bunch",
-        image: "https://udemy-images.udemy.com/course/100x100/2195280_49b2.jpg"
+        image: "https://img-c.udemycdn.com/course/240x135/2195280_49b2_2.jpg"
     },
     {
         course: "Apache Kafka Series",
         instructor: "Stephane Maarek",
-        image: "https://udemy-images.udemy.com/course/480x270/1141696_6151_3.jpg"
+        image: "https://img-c.udemycdn.com/course/240x135/1075642_b6d2_9.jpg"
     },
     {
         course: "Music Production in Logic Pro X",
         instructor: "Tomas George",
-        image: "https://udemy-images.udemy.com/course/480x270/897192_2cee_6.jpg"
+        image: "https://img-c.udemycdn.com/course/240x135/897192_2cee_7.jpg"
 
     },
     {
         course: "Unity Game Development",
         instructor: "Jonathan Weinberger",
-        image: "https://udemy-images.udemy.com/course/480x270/1328572_b05d_4.jpg"
+        image: "https://img-c.udemycdn.com/course/240x135/1328572_b05d_5.jpg"
     }
 ]
 
